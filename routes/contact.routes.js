08383a1fa63fd1ab7742cd9758bc8e809734fdbf
Rosine22 <r.uwineza@alustudent.com>
2024-05-07 09:22:42 +0200

    new

--- routes/contact.routes.js
+++ routes/contact.routes.js
@@ -5,7 +5,7 @@ import contactController from '../controllers/contact.controller.js'
 import contactModel from '../models/contact.model.js'
 
 router.post('/create' , contactController.createContact)
-router.get('/list' , contactController.getAllContact)
+router.get('/lists' , contactController.getAllContact)
 router.get('/:id' , contactController.getContact)
 router.put('/update/:id' , contactController.updateContact)
 router.delete('/delete/:id', contactController.deleteContact)
